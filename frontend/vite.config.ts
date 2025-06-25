b06743284b4f24b3951a823cfebedc23f66b1626
Nipat <nipatchapakdee@gmail.com>
2025-06-25 22:27:11 +0700

    new config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://nginx-proxy:8080',
+        target: 'http://backend:8000',
         changeOrigin: true,
         secure: false,
       },
